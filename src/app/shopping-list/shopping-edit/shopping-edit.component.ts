8b3e0962f168ba6f4ef0024c7bf9c53ec2797eb7
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-11-14 23:56:51 +0530

    added functionality for clear button for shopping list edit component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,14 +33,21 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  onAddItem(forms: NgForm) {
-    const value = forms.value;
+  onSubmit(form: NgForm) {
+    const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if(this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient);
     }else{
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy() {
