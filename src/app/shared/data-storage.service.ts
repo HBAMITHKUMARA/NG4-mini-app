8d38b14f5672879ffdeb42c3100668de58626829
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-11-19 23:00:43 +0530

    data-storage service configured to coonect db

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor() { }
+  constructor(private http: Http, private recipeService: RecipeService) { }
 
+  storeRecipes() {
+    return this.http.put('https://ami-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
 }
