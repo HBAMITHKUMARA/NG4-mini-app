0fdf8bd11b4eaf74c7bb0a1deac5c900142c72dd
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-11-21 23:44:22 +0530

    configured signin component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,7 +9,17 @@ export class AuthService {
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
-        error => console.log()
+        error => console.log(error)
+      );
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
+      )
+      .catch(
+        error => console.log(error)
       );
   }
 
