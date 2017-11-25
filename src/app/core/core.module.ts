cc0afd433cbc5d019dd0f176b2ff8763946aae16
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-11-25 14:44:27 +0530

    Code refactor for providers

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,24 @@ import { HeaderComponent } from 'app/core/header/header.component';
 import { HomeComponent } from 'app/core/home/home.component';
 import { SharedModule } from 'app/shared/shared.module';
 import { AppRoutingModule } from 'app/app-routing.module';
+import { ShoppingListService } from 'app/shopping-list/shopping-list.service';
+import { RecipeService } from 'app/recipes/recipe.service';
+import { DataStorageService } from 'app/shared/data-storage.service';
+import { AuthService } from 'app/auth/auth.service';
+import { AuthGuardService } from 'app/auth/auth-guard.service';
 
 @NgModule({
     declarations: [
         HeaderComponent,
         HomeComponent
     ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        DataStorageService,
+        AuthService,
+        AuthGuardService
+      ],
     imports: [
         CommonModule,
         SharedModule,
