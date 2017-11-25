917211b69e5b867c8d930b9ed40bc9f62e3917cc
HBAMITHKUMARA <amithkumar.hb94@gmail.com>
2017-11-25 14:10:32 +0530

    minor bug fix for canLoad feature

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanLoad, Route } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.authService.isAuthenticated();
+  }
+
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.isAuthenticated();
   }
 
